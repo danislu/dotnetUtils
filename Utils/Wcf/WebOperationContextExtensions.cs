0da560168656ceb1c759547b397d920ffae2b692
Sofapute <danislu@gmail.com>
2011-12-26 22:13:44 +0100

    added extenstions

---
+++ Utils/Wcf/WebOperationContextExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils.Wcf
+{
+    public static class WebOperationContextExtensions
+    {
+        public static string GetQueryParam(this IWebOperationContext context, string key)
+        {
+            return context.IncomingRequest.GetQueryParameter(key);
+        }
+    }
+}
