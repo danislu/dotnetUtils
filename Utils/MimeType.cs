0ba93a301f7c9feddfcb38764a04a30cc0156792
Sofapute <danislu@gmail.com>
2011-12-26 22:15:31 +0100

    updated mimetypes

--- Utils/MimeType.cs
+++ Utils/MimeType.cs
@@ -1,8 +1,21 @@
 ﻿namespace Utils
 {
+    /// <summary>
+    /// http://www.iana.org/assignments/media-types/index.html
+    /// </summary>
     public static class MimeTypes
     {
+        public static readonly string Pain = "text/plain";
+        public static readonly string Html = "text/html";
+
+        public static readonly string Json = "application/json";
+
+        public static readonly string Xml = "application/xml";
+        public static readonly string Rss = "application/rss+xml";
+        public static readonly string Atom = "application/atom+xml";
+        public static readonly string AtomFeed = Atom + ";type=feed";
+        public static readonly string AtomEntry = Atom + ";type=entry";
+        public static readonly string AtomCat = "application/atomcat+xml";
         public static readonly string AtomPub = "application/atomsvc+xml";
-        public static readonly string AtomFeed = "application/atom+xml";
     }
 }
