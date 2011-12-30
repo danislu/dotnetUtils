d978a6dc861cd57997d52920543a0db14760cec6
Sofapute <danislu@gmail.com>
2011-12-30 11:26:11 +0100

    added sln

--- Utils/Tap/IProgress.cs
+++ Utils/Tap/IProgress.cs
@@ -1,5 +1,10 @@
 ﻿namespace System.Threading
 {
+    /// <summary>
+    /// Interface included in .Net 4.5
+    /// Use this one util it is released.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IProgress<in T>
     {
         void Report(T value);
