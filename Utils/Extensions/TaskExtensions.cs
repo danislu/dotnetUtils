559abcff4d11b7abeb38994496bc0064340badfc
Sofapute <danislu@gmail.com>
2012-01-11 21:09:04 +0100

    fixed some typeos and added some task extenstions

---
+++ Utils/Extensions/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Threading.Tasks
+{
+    using System;
+
+    public static class TaskExtensions
+    {
+        public static Task ContinueWithOnCurrentSynchronizationContext(this Task task, Action<Task> action)
+        {
+            var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            return task.ContinueWith(action, scheduler);
+        }
+
+        public static Task ContinueWithOnCurrentSynchronizationContext<T>(this Task<T> task, Action<Task<T>> action)
+        {
+            var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            return task.ContinueWith(action, scheduler);
+        }
+    }
+}
