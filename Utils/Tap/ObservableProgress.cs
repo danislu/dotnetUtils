5c4427c7ed3769976243fcacf73e8eaf52920076
Sofapute <danislu@gmail.com>
2012-10-28 21:27:39 -0700

    fixed obs prog, and added test

--- Utils/Tap/ObservableProgress.cs
+++ Utils/Tap/ObservableProgress.cs
@@ -1,27 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Utils.Tap
+﻿namespace Utils.Tap
 {
     using System.Reactive.Linq;
     using System.Threading;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
     public class ObservableProgress<T> : IProgress<T>
     {
-        public static event EventHandler<ProgressReportedEventArgs<T>> ProgressReported;
+        IObservable<ProgressReportedEventArgs<T>> observable;
+
+        public event EventHandler<ProgressReportedEventArgs<T>> ProgressReported;
 
         public IObservable<ProgressReportedEventArgs<T>> Progress
         {
-            get { return null; }
+            get
+            {
+                return observable ?? (observable = CreateObservable());
+            }
         }
 
-        public ObservableProgress()
+        private IObservable<ProgressReportedEventArgs<T>> CreateObservable()
         {
-            //var a = Observable.FromEvent(
+            return Observable
+                .FromEventPattern<ProgressReportedEventArgs<T>>(h => this.ProgressReported += h, h => this.ProgressReported -= h)
+                .Select(t => t.EventArgs as ProgressReportedEventArgs<T>);
         }
-        
+
         public void Report(T value)
         {
             ProgressReported(this, new ProgressReportedEventArgs<T>(value));
---
+++ Utils/Tests/ObservableProgressTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Utils.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Utils.Tap;
+
+    [TestFixture]
+    class ObservableProgressTest
+    {
+        [Test]
+        public void ProgressTest()
+        {
+            var observableProgress = new ObservableProgress<int>();
+
+            var queue = new Queue<int>();
+            var observable = observableProgress.Progress;
+            observable.Subscribe(onNext: arg => queue.Enqueue(arg.Value));
+
+            for (int i = 0; i < 10; i++)
+            {
+                observableProgress.Report(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(i, queue.Dequeue());
+            }
+        }
+    }
+}
