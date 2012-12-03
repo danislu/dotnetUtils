2696da5ce6b70b9b4a4f8d60354303627f7982fb
Sofapute <danislu@gmail.com>
2012-12-03 22:11:44 +0100

    added wpf utils

---
+++ Utils.WPF.Tests/OverrideCursorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NUnit.Framework;
+
+namespace Utils.WPF.Tests
+{
+    [TestFixture]
+    public class OverrideCursorTests
+    {
+        [STAThread]
+        [Test]
+        public void TestSetOverrideCursor()
+        {
+            using(new OverrideCursor(Cursors.Cross))
+            {
+                Assert.AreEqual(Cursors.Cross, Mouse.OverrideCursor);
+            }
+            Assert.IsNull(Mouse.OverrideCursor);
+        }
+
+        [STAThread]
+        [Test]
+        public void TestChangeOverrideCursor()
+        {
+            Mouse.OverrideCursor = Cursors.Hand;
+            using (new OverrideCursor(Cursors.Cross))
+            {
+                Assert.AreEqual(Cursors.Cross, Mouse.OverrideCursor);
+            }
+            Assert.AreEqual(Cursors.Hand, Mouse.OverrideCursor);
+        }
+    }
+}
---
+++ Utils.WPF/OverrideCursor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Utils.WPF
+{
+    public class OverrideCursor : IDisposable
+    {
+        private readonly Cursor oldOverrideCursor;
+
+        public OverrideCursor(Cursor newCursor)
+        {
+            oldOverrideCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = newCursor;
+        }
+
+        public void Dispose()
+        {
+            Mouse.OverrideCursor = oldOverrideCursor;
+        }
+    }
+}
